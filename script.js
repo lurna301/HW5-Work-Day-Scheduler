d429752e1302626b0deb6b71a9961702433bf8c6
Lauren A <laurenagraphicdesign@gmail.com>
2022-04-15 00:32:11 -0400

    added variables, needs work

---
+++ script.js
@@ -0,0 +1,20 @@
+var getTime = new Date();
+var myMonth = getTime.getMonth();
+var myDate = getTime.getDate();
+var myDay = getTime.getDay();
+
+var monthList = [months = 
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+    ];
+console.log (monthList[myMonth])
